7022a0d6ee0804f53c60b17b1f1e3c95a434e61a
Martin Zeman (Zemistr) <me@zemistr.eu>
2016-10-31 09:02:41 +0100

    Add updated version for PHP7

--- src/ReflectionClass.php
+++ src/ReflectionClass.php
@@ -1,40 +1,50 @@
 <?php
+declare(strict_types = 1);
+
 namespace Yep\Reflection;
 
-class ReflectionClass extends \ReflectionClass {
-	/** @var object */
-	protected $object;
+class ReflectionClass extends \ReflectionClass
+{
+    /** @var object */
+    protected $object;
 
-	public function __construct($object) {
-		if (!is_object($object)) {
-			throw new \InvalidArgumentException(sprintf('Expected "object", got "%s".', gettype($object)));
-		}
+    public function __construct($object)
+    {
+        if (!is_object($object)) {
+            throw new \InvalidArgumentException(sprintf('Expected "object", got "%s".', gettype($object)));
+        }
 
-		parent::__construct($object);
-		$this->object = $object;
-	}
+        parent::__construct($object);
+        $this->object = $object;
+    }
 
-	public static function from($class) {
-		return new static($class);
-	}
+    public static function from($class): self
+    {
+        return new static($class);
+    }
 
-	public function invokeMethod($method, array $arguments = []) {
-		$reflection = $this->getMethod($method);
-		$reflection->setAccessible(true);
+    public function invokeMethod(string $method, array $arguments = [])
+    {
+        $reflection = $this->getMethod($method);
+        $reflection->setAccessible(true);
 
-		return $reflection->invokeArgs($this->object, $arguments);
-	}
+        return $reflection->invokeArgs($this->object, $arguments);
+    }
 
-	public function setPropertyValue($property, $value) {
-		$reflection = $this->getProperty($property);
-		$reflection->setAccessible(true);
-		$reflection->setValue($this->object, $value);
-	}
+    public function setPropertyValue(string $property, $value): self
+    {
+        $reflection = $this->getProperty($property);
+        $reflection->setAccessible(true);
+        $reflection->setValue($this->object, $value);
 
-	public function getPropertyValue($property) {
-		$reflection = $this->getProperty($property);
-		$reflection->setAccessible(true);
+        return $this;
+    }
 
-		return $reflection->getValue($this->object);
-	}
+    public function getPropertyValue(string $property)
+    {
+        $reflection = $this->getProperty($property);
+        $reflection->setAccessible(true);
+
+        return $reflection->getValue($this->object);
+    }
 }
--- tests/ReflectionClassTest.php
+++ tests/ReflectionClassTest.php
@@ -1,87 +1,102 @@
 <?php
+declare(strict_types = 1);
+
 use Yep\Reflection\ReflectionClass;
 
-class ReflectionClassTest extends PHPUnit_Framework_TestCase {
-	public function testConstructor() {
-		$class = new TestClass();
+class ReflectionClassTest extends PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $class = new TestClass();
 
-		$reflection_class = new ReflectionClass($class);
-		$reflection_property = new ReflectionProperty($reflection_class, 'object');
-		$reflection_property->setAccessible(true);
+        $reflection_class = new ReflectionClass($class);
+        $reflection_property = new ReflectionProperty($reflection_class, 'object');
+        $reflection_property->setAccessible(true);
 
-		$this->assertSame($class, $reflection_property->getValue($reflection_class));
-	}
+        $this->assertSame($class, $reflection_property->getValue($reflection_class));
+    }
 
-	/**
-	 * @expectedException \InvalidArgumentException
-	 */
-	public function testConstructorWithException() {
-		new ReflectionClass('TestClass');
-	}
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testConstructorWithException()
+    {
+        new ReflectionClass('TestClass');
+    }
 
-	public function testFrom() {
-		$class = new TestClass();
+    public function testFrom()
+    {
+        $class = new TestClass();
 
-		$reflection_class = ReflectionClass::from($class);
-		$reflection_property = new ReflectionProperty($reflection_class, 'object');
-		$reflection_property->setAccessible(true);
+        $reflection_class = ReflectionClass::from($class);
+        $reflection_property = new ReflectionProperty($reflection_class, 'object');
+        $reflection_property->setAccessible(true);
 
-		$this->assertSame($class, $reflection_property->getValue($reflection_class));
-	}
+        $this->assertSame($class, $reflection_property->getValue($reflection_class));
+    }
 
-	/**
-	 * @expectedException \InvalidArgumentException
-	 */
-	public function testFromWithException() {
-		ReflectionClass::from('TestClass');
-	}
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFromWithException()
+    {
+        ReflectionClass::from('TestClass');
+    }
 
-	public function testInvokeMethod() {
-		$class = new TestClass();
+    public function testInvokeMethod()
+    {
+        $class = new TestClass();
 
-		$this->assertSame('protected:foo', ReflectionClass::from($class)->invokeMethod('testProtectedMethod', ['foo']));
-		$this->assertSame('protected:foo#bar', ReflectionClass::from($class)->invokeMethod('testProtectedMethod', ['foo', 'bar']));
+        $this->assertSame('protected:foo', ReflectionClass::from($class)->invokeMethod('testProtectedMethod', ['foo']));
+        $this->assertSame('protected:foo#bar', ReflectionClass::from($class)->invokeMethod('testProtectedMethod', ['foo', 'bar']));
 
-		$this->assertSame('private:foo', ReflectionClass::from($class)->invokeMethod('testPrivateMethod', ['foo']));
-		$this->assertSame('private:foo#bar', ReflectionClass::from($class)->invokeMethod('testPrivateMethod', ['foo', 'bar']));
-	}
+        $this->assertSame('private:foo', ReflectionClass::from($class)->invokeMethod('testPrivateMethod', ['foo']));
+        $this->assertSame('private:foo#bar', ReflectionClass::from($class)->invokeMethod('testPrivateMethod', ['foo', 'bar']));
+    }
 
-	public function testSetPropertyValue() {
-		$class = new TestClass();
+    public function testSetPropertyValue()
+    {
+        $class = new TestClass();
 
-		$this->assertNull($class->getTestProperty());
-		ReflectionClass::from($class)->setPropertyValue('test_property', 'foo');
-		$this->assertSame('foo', $class->getTestProperty());
-	}
+        $this->assertNull($class->getTestProperty());
+        ReflectionClass::from($class)->setPropertyValue('test_property', 'foo');
+        $this->assertSame('foo', $class->getTestProperty());
+    }
 
-	public function testGetPropertyValue() {
-		$class = new TestClass();
+    public function testGetPropertyValue()
+    {
+        $class = new TestClass();
 
-		$this->assertNull($class->getTestProperty());
-		$this->assertNull(ReflectionClass::from($class)->getPropertyValue('test_property'));
+        $this->assertNull($class->getTestProperty());
+        $this->assertNull(ReflectionClass::from($class)->getPropertyValue('test_property'));
 
-		$class->setTestProperty('foo');
+        $class->setTestProperty('foo');
 
-		$this->assertSame('foo', ReflectionClass::from($class)->getPropertyValue('test_property'));
-	}
+        $this->assertSame('foo', ReflectionClass::from($class)->getPropertyValue('test_property'));
+    }
 }
 
-class TestClass {
-	protected $test_property;
+class TestClass
+{
+    protected $test_property;
 
-	protected function testProtectedMethod() {
-		return 'protected:' . implode('#', func_get_args());
-	}
+    protected function testProtectedMethod()
+    {
+        return 'protected:'.implode('#', func_get_args());
+    }
 
-	private function testPrivateMethod() {
-		return 'private:' . implode('#', func_get_args());
-	}
+    private function testPrivateMethod()
+    {
+        return 'private:'.implode('#', func_get_args());
+    }
 
-	public function getTestProperty() {
-		return $this->test_property;
-	}
+    public function getTestProperty()
+    {
+        return $this->test_property;
+    }
 
-	public function setTestProperty($test_property) {
-		$this->test_property = $test_property;
-	}
+    public function setTestProperty($test_property)
+    {
+        $this->test_property = $test_property;
+    }
 }
