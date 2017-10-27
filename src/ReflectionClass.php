f90ba085212230bf17c5d820bc89899afa282649
Martin Zeman (Zemistr) <me@zemistr.eu>
2017-10-27 23:39:16 +0200

    Add support for parent's reflection

--- src/ReflectionClass.php
+++ src/ReflectionClass.php
@@ -5,17 +5,25 @@ class ReflectionClass extends \ReflectionClass {
 	/** @var object */
 	protected $object;
 
-	public function __construct($object) {
+	public function __construct($class, $object = null) {
+		if (is_object($class) && !is_object($object)) {
+			$object = $class;
+		}
+
 		if (!is_object($object)) {
 			throw new \InvalidArgumentException(sprintf('Expected "object", got "%s".', gettype($object)));
 		}
 
-		parent::__construct($object);
+		parent::__construct($class);
 		$this->object = $object;
 	}
 
-	public static function from($class) {
-		return new static($class);
+	public function getObject() {
+		return $this->object;
+	}
+
+	public static function from($class, $object = null) {
+		return new static($class, $object);
 	}
 
 	public function invokeMethod($method, array $arguments = []) {
@@ -29,6 +37,8 @@ class ReflectionClass extends \ReflectionClass {
 		$reflection = $this->getProperty($property);
 		$reflection->setAccessible(true);
 		$reflection->setValue($this->object, $value);
+
+		return $this;
 	}
 
 	public function getPropertyValue($property) {
@@ -36,5 +46,9 @@ class ReflectionClass extends \ReflectionClass {
 		$reflection->setAccessible(true);
 
 		return $reflection->getValue($this->object);
+	}
+
+	public function getParent() {
+		return self::from($this->getParentClass()->getName(), $this->object);
 	}
 }
--- tests/ReflectionClassTest.php
+++ tests/ReflectionClassTest.php
@@ -2,14 +2,28 @@
 use Yep\Reflection\ReflectionClass;
 
 class ReflectionClassTest extends PHPUnit_Framework_TestCase {
+	private function getPropertyValue(\ReflectionClass $ref, $name) {
+		$property = new ReflectionProperty($ref, $name);
+		$property->setAccessible(true);
+
+		return $property->getValue($ref);
+	}
+
 	public function testConstructor() {
 		$class = new TestClass();
 
-		$reflection_class = new ReflectionClass($class);
-		$reflection_property = new ReflectionProperty($reflection_class, 'object');
-		$reflection_property->setAccessible(true);
+		$reflection = new ReflectionClass($class);
+		$this->assertSame(
+			$class,
+			$this->getPropertyValue($reflection, 'object')
+		);
 
-		$this->assertSame($class, $reflection_property->getValue($reflection_class));
+		$class2 = new TestClass2();
+		$reflection = new ReflectionClass($class, $class2);
+		$this->assertSame(
+			$class2,
+			$this->getPropertyValue($reflection, 'object')
+		);
 	}
 
 	/**
@@ -19,14 +33,28 @@ class ReflectionClassTest extends PHPUnit_Framework_TestCase {
 		new ReflectionClass('TestClass');
 	}
 
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testConstructorWithExceptionOnObject() {
+		new ReflectionClass('TestClass', 'TestClass');
+	}
+
 	public function testFrom() {
 		$class = new TestClass();
 
-		$reflection_class = ReflectionClass::from($class);
-		$reflection_property = new ReflectionProperty($reflection_class, 'object');
-		$reflection_property->setAccessible(true);
+		$reflection = ReflectionClass::from($class);
+		$this->assertSame(
+			$class,
+			$this->getPropertyValue($reflection, 'object')
+		);
 
-		$this->assertSame($class, $reflection_property->getValue($reflection_class));
+		$class2 = new TestClass2();
+		$reflection = ReflectionClass::from($class, $class2);
+		$this->assertSame(
+			$class2,
+			$this->getPropertyValue($reflection, 'object')
+		);
 	}
 
 	/**
@@ -36,38 +64,77 @@ class ReflectionClassTest extends PHPUnit_Framework_TestCase {
 		ReflectionClass::from('TestClass');
 	}
 
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testFromWithExceptionOnObject() {
+		ReflectionClass::from('TestClass', 'TestClass');
+	}
+
 	public function testInvokeMethod() {
 		$class = new TestClass();
+		$reflection = ReflectionClass::from($class);
 
-		$this->assertSame('protected:foo', ReflectionClass::from($class)->invokeMethod('testProtectedMethod', ['foo']));
-		$this->assertSame('protected:foo#bar', ReflectionClass::from($class)->invokeMethod('testProtectedMethod', ['foo', 'bar']));
-
-		$this->assertSame('private:foo', ReflectionClass::from($class)->invokeMethod('testPrivateMethod', ['foo']));
-		$this->assertSame('private:foo#bar', ReflectionClass::from($class)->invokeMethod('testPrivateMethod', ['foo', 'bar']));
+		$this->assertSame(
+			'protected:foo',
+			$reflection->invokeMethod('testProtectedMethod', ['foo'])
+		);
+		$this->assertSame(
+			'protected:foo#bar',
+			$reflection->invokeMethod('testProtectedMethod', ['foo', 'bar'])
+		);
+		$this->assertSame(
+			'private:foo',
+			$reflection->invokeMethod('testPrivateMethod', ['foo'])
+		);
+		$this->assertSame(
+			'private:foo#bar',
+			$reflection->invokeMethod('testPrivateMethod', ['foo', 'bar'])
+		);
 	}
 
 	public function testSetPropertyValue() {
 		$class = new TestClass();
 
-		$this->assertNull($class->getTestProperty());
-		ReflectionClass::from($class)->setPropertyValue('test_property', 'foo');
+		$this->assertSame(1, $class->getTestProperty());
+		ReflectionClass::from($class)->setPropertyValue('testProperty', 'foo');
 		$this->assertSame('foo', $class->getTestProperty());
 	}
 
 	public function testGetPropertyValue() {
 		$class = new TestClass();
+		$reflection = ReflectionClass::from($class);
 
-		$this->assertNull($class->getTestProperty());
-		$this->assertNull(ReflectionClass::from($class)->getPropertyValue('test_property'));
+		$this->assertSame(1, $class->getTestProperty());
+		$this->assertSame(1, $reflection->getPropertyValue('testProperty'));
 
 		$class->setTestProperty('foo');
 
-		$this->assertSame('foo', ReflectionClass::from($class)->getPropertyValue('test_property'));
+		$this->assertSame(
+			'foo',
+			$reflection->getPropertyValue('testProperty')
+		);
+	}
+
+	public function testParent() {
+		$class = new TestClass3();
+		$reflection = ReflectionClass::from($class);
+
+		$class->setTestProperty(1);
+		$this->assertSame(3, $reflection->getPropertyValue('testProperty'));
+
+		$reflection = $reflection->getParent();
+		$this->assertInstanceOf(ReflectionClass::class, $reflection);
+		$this->assertSame(2, $reflection->getPropertyValue('testProperty'));
+
+		$reflection = $reflection->getParent();
+		$this->assertInstanceOf(ReflectionClass::class, $reflection);
+		$this->assertSame(1, $reflection->getPropertyValue('testProperty'));
 	}
 }
 
 class TestClass {
-	protected $test_property;
+	private $testProperty = 1;
 
 	protected function testProtectedMethod() {
 		return 'protected:' . implode('#', func_get_args());
@@ -78,10 +145,18 @@ class TestClass {
 	}
 
 	public function getTestProperty() {
-		return $this->test_property;
+		return $this->testProperty;
 	}
 
-	public function setTestProperty($test_property) {
-		$this->test_property = $test_property;
+	public function setTestProperty($testProperty) {
+		$this->testProperty = $testProperty;
 	}
+}
+
+class TestClass2 extends TestClass {
+	private $testProperty = 2;
+}
+
+class TestClass3 extends TestClass2 {
+	private $testProperty = 3;
 }
